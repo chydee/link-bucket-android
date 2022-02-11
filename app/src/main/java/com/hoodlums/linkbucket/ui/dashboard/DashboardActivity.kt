b963d1871be661985af06190929e1515ac30c06d
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2022-02-11 08:20:01 +0100

    added assets for the user avatar and the dashboard bottom navigation

---
+++ app/src/main/java/com/hoodlums/linkbucket/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.hoodlums.linkbucket.ui.dashboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hoodlums.linkbucket.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
