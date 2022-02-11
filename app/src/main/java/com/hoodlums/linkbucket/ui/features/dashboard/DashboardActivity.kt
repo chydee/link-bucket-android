4bcdcf3ac2c2236aa7916bf664c81618b6dc22db
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2022-02-11 09:13:38 +0100

    setup the Dashboard Bottom Navigation

--- app/src/main/java/com/hoodlums/linkbucket/ui/features/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/hoodlums/linkbucket/ui/features/dashboard/DashboardActivity.kt
@@ -2,11 +2,35 @@ package com.hoodlums.linkbucket.ui.features.dashboard
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.hoodlums.linkbucket.R
+import com.hoodlums.linkbucket.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater).apply {
+            setContentView(this.root)
+        }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+
+        binding.bottomNavigationView.background = null
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+                || super.onSupportNavigateUp()
     }
 }
